558a88ebff4d1abcbd456558642311199ffcbf01
Ella van Durpe <ella@vandurpe.com>
2021-02-24 20:18:23 +0200

    Attempt to deprecate useBlockEditContext in favour of useBlockClientId

--- packages/block-editor/src/components/block-edit/context.js
+++ packages/block-editor/src/components/block-edit/context.js
@@ -1,23 +1,18 @@
 /**
- * External dependencies
- */
-import { noop } from 'lodash';
-
-/**
  * WordPress dependencies
  */
+import { useSelect } from '@wordpress/data';
 import { createContext, useContext } from '@wordpress/element';
 
-const Context = createContext( {
-	name: '',
-	isSelected: false,
-	focusedElement: null,
-	setFocusedElement: noop,
-	clientId: null,
-} );
+/**
+ * Internal dependencies
+ */
+import { store as blockEditorStore } from '../../store';
+
+const Context = createContext();
 const { Provider } = Context;
 
-export { Provider as BlockEditContextProvider };
+export { Provider as BlockClientIdProvider };
 
 /**
  * A hook that returns the block edit context.
@@ -25,5 +20,18 @@ export { Provider as BlockEditContextProvider };
  * @return {Object} Block edit context
  */
 export function useBlockEditContext() {
-	return useContext( Context );
+	const clientId = useContext( Context );
+	return useSelect(
+		( select ) => {
+			const { getBlockName, isBlockSelected } = select(
+				blockEditorStore
+			);
+			return {
+				clientId,
+				name: getBlockName( clientId ),
+				isSelected: isBlockSelected( clientId ),
+			};
+		},
+		[ clientId ]
+	);
 }
